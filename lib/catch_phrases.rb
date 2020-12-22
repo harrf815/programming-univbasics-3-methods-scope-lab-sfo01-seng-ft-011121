ab9d992773367bad44b4c0c4000d345fd767bff2
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-22 23:21:57 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,12 +4,12 @@ def mario
 end
 
 def toadstool
-  status = "'Thank You Mario! But Our Princess Is In Another Castle!'"
+  status = "Thank You Mario! But Our Princess Is In Another Castle!"
   puts status
 end
 
 def link
-  status = "'It's Dangerous To Go Alone! Take This.'"
+  status = "It's Dangerous To Go Alone! Take This."
   puts status
 end  
   
